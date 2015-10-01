35207c8cbc24f334b77f87c74e87f181b855338e
qingweilan <qingweilan@gmail.com>
2015-10-01 15:32:54 -0700

    Completed input reformatting

--- lab1a/lab1/read-command.c
+++ lab1a/lab1/read-command.c
@@ -9,8 +9,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#include <assert.h>
-
 /* Helper functions with tests */
 
 typedef enum token_type
@@ -36,16 +34,6 @@ typedef struct token
 } token;
 
 typedef token *token_t;
-
-
-// token_t get_token (char *word, token_type type, int *is_word);
-// void print_token (token_t token);
-// void clean_up_token (token_t * token, char **word, FILE ** stream,
-// 		     size_t * size);
-
-
-/* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
@@ -91,11 +79,15 @@ make_command_stream (int (*get_next_byte) (void *),
 	int comment_flag = 0;
 
 	unsigned int length = strlen (buffer);
-	char *script = (char *) malloc (2*length);
+	char *script = (char *) malloc (3*length);
 	unsigned int pos = 0;
 	unsigned int i = 0;
 	for (; i < length; i++)
 	{
+		// realloc if script is full
+		if (pos >= length-10)
+			script = (char *) realloc (script, 4*length);
+
 		int cc = buffer[i];
 
 		if (comment_flag)
@@ -218,11 +210,146 @@ make_command_stream (int (*get_next_byte) (void *),
 	script[pos] = '\0';
 
 	fprintf(stderr, "%s\n", script);
+	fprintf(stderr, "---------------------------------------\n");
+
+	/************ trim extra spaces ************/
+	free (buffer);
+	length = strlen (script);
+	buffer = (char *) malloc (2*length);
+	pos = 0;
+	comment_flag = 0;
+	for (i = 0; i < length; i++)
+	{
+		int cc = script[i];
+
+		if (comment_flag)
+		{
+			if (cc != '\n')
+			{
+				buffer[pos] = cc;
+				pos++;
+				continue;
+			}
+			else
+			{
+				buffer[pos] = cc;
+				pos++;
+				comment_flag = 0;
+				continue;
+			}
+		}
+
+		if (cc == '#')
+		{
+			comment_flag = 1;
+			buffer[pos] = cc;
+			pos++;
+		}
+
+		else if (cc == ' ')
+		{
+			if (buffer[pos-1] == ' ' || buffer[pos-1] == '\n')
+			{
+				continue;
+			}
+			else
+			{
+				buffer[pos] = cc;
+				pos++;
+			}
+		}
+
+		else
+		{
+			buffer[pos] = cc;
+			pos++;
+		}
+
+	}
+
+	buffer[pos] = '\0';
+	fprintf(stderr, "%s\n", buffer);
+	fprintf(stderr, "---------------------------------------\n");
 
 
-	/************ trim extra spaces & newlines ************/
 
+	/************ trim extra newlines ************/
+	free (script);
+	length = strlen (buffer);
+	script = (char *) malloc (2*length);
+	pos = 0;
+	comment_flag = 0;
+	int special_flag = 0;
+	for (i = 0; i < length; i++)
+	{
+		int cc = buffer[i];
 
+		if (comment_flag)
+		{
+			if (cc != '\n')
+			{
+				script[pos] = cc;
+				pos++;
+				continue;
+			}
+			else
+			{
+				script[pos] = cc;
+				pos++;
+				comment_flag = 0;
+				continue;
+			}
+			special_flag = 0;
+		}
+
+		if (cc == '#')
+		{
+			comment_flag = 1;
+			script[pos] = cc;
+			pos++;
+			special_flag = 0;
+		}
+
+		else if (cc == '&' && script[pos-1] == '&')
+		{
+			special_flag = 1;
+			script[pos] = cc;
+			pos++;
+		}
+
+		else if (cc == '|')
+		{
+			special_flag = 1;
+			script[pos] = cc;
+			pos++;
+		}
+
+		else if (cc == '\n')
+		{
+			if (special_flag && is_valid (buffer[pos+1]))
+			{
+				script[pos] = ' ';
+				pos++;
+			}
+			else
+			{
+				script[pos] = cc;
+				pos++;
+			}
+			special_flag = 0;
+		}
+
+		else
+		{
+			script[pos] = cc;
+			pos++;
+			special_flag = 0;
+		}
+	}
+
+	script[pos] = '\0';
+	fprintf(stderr, "%s\n", script);
+	fprintf(stderr, "---------------------------------------\n");
 
 	free (buffer);
 	free (script);
@@ -386,419 +513,3 @@ get_token_type (char *word)
 //   fseeko (*stream, 0, SEEK_SET);
 //   // fprintf(stderr, "WORD: %s\n", *word);
 // }
-
-// FILE *stream;
-//   size_t size;
-//   char *word;
-//   stream = open_memstream (&word, &size);
-
-//   int is_special = 0;
-//   int is_new_line = 0;
-//   int is_word = 0;
-//   int is_comment = 0;
-//   int special_char;
-
-//   int c;
-//   while ((c = (*get_next_byte) (get_next_byte_argument)) != EOF)
-//     {
-//       // fprintf (stderr, "byte: %c\n", c);
-//       if (c == '#')
-// 	{
-// 	  is_comment = 1;
-// 	  fprintf (stream, "%c", c);
-// 	  fflush (stream);
-// 	}
-//       else if (!is_comment || c == '\n')
-// 	{
-// 	  if (c != '\n' && c != ' ')
-// 	    {
-// 	      if (is_special)
-// 		{
-// 		  if (special_char == c)
-// 		    {
-// 		      token_t token = get_token (word, get_token_type (word),
-// 						 &is_word);
-// 		      print_token (token);
-// 		      clean_up_token (&token, &word, &stream, &size);
-
-// 		      fprintf (stream, "%c%c", special_char, c);
-// 		      fflush (stream);
-
-// 		      token =
-// 			get_token (word, get_token_type (word), &is_word);
-// 		      print_token (token);
-// 		      clean_up_token (&token, &word, &stream, &size);
-
-// 		      is_special = 0;
-// 		    }
-// 		  else
-// 		    {
-// 		      if (special_char == '|')
-// 			{
-// 			  token_t token =
-// 			    get_token (word, get_token_type (word),
-// 				       &is_word);
-// 			  print_token (token);
-// 			  clean_up_token (&token, &word, &stream, &size);
-
-// 			  fprintf (stream, "%c", special_char);
-// 			  fflush (stream);
-
-// 			  token =
-// 			    get_token (word, get_token_type (word), &is_word);
-// 			  print_token (token);
-// 			  clean_up_token (&token, &word, &stream, &size);
-// 			}
-// 		      else
-// 			{
-// 			  fprintf (stream, "%c%c", special_char, c);
-// 			  fflush (stream);
-// 			}
-
-// 		      is_special = 0;
-// 		    }
-// 		}
-// 	      else if (c == '&' || c == '|')
-// 		{
-// 		  is_special = 1;
-// 		  special_char = c;
-// 		}
-// 	      else if (c == '<' ||
-// 		       c == '>' || c == '(' || c == ')' || c == ';')
-// 		{
-// 		  token_t token =
-// 		    get_token (word, get_token_type (word), &is_word);
-// 		  print_token (token);
-// 		  clean_up_token (&token, &word, &stream, &size);
-
-// 		  fprintf (stream, "%c", c);
-// 		  fflush (stream);
-
-// 		  token = get_token (word, get_token_type (word), &is_word);
-// 		  print_token (token);
-// 		  clean_up_token (&token, &word, &stream, &size);
-// 		}
-// 	      else
-// 		{
-// 		  fprintf (stream, "%c", c);
-// 		  fflush (stream);
-// 		}
-// 	      is_new_line = 0;
-// 	    }
-
-// 	  else if (c == '\n')
-// 	    {
-// 	      if (is_comment)
-// 		{
-// 		  token_t token = get_token (word, TOKEN_COMMENT, &is_word);
-// 		  print_token (token);
-// 		  clean_up_token (&token, &word, &stream, &size);
-// 		  is_comment = 0;
-// 		  fprintf (stderr, "Got here\n");
-// 		}
-// 	      else if (is_new_line)
-// 		{
-// 		  if (is_special)
-// 		    {
-// 		      token_t token = get_token (word, get_token_type (word),
-// 						 &is_word);
-// 		      print_token (token);
-// 		      clean_up_token (&token, &word, &stream, &size);
-
-// 		      fprintf (stream, "%c", special_char);
-// 		      fflush (stream);
-// 		      is_special = 0;
-// 		    }
-
-// 		  token_t token =
-// 		    get_token (word, get_token_type (word), &is_word);
-// 		  print_token (token);
-// 		  clean_up_token (&token, &word, &stream, &size);
-
-// 		  if (is_word)
-// 		    {
-// 		      token = get_token ("", TOKEN_SEPARATOR, &is_word);
-// 		      print_token (token);
-// 		      clean_up_token (&token, &word, &stream, &size);
-
-// 		      is_new_line = 0;
-// 		    }
-// 		  else
-// 		    {
-
-// 		    }
-// 		}
-// 	      else
-// 		{
-// 		  is_new_line = 1;
-// 		}
-// 	    }
-
-// 	  else
-// 	    {
-// 	      if (is_special)
-// 		{
-// 		  fprintf (stream, "%c", special_char);
-// 		  fflush (stream);
-// 		  is_special = 0;
-// 		}
-// 	      if (strlen (word) > 0)
-// 		{
-// 		  token_t token =
-// 		    get_token (word, get_token_type (word), &is_word);
-// 		  print_token (token);
-// 		  clean_up_token (&token, &word, &stream, &size);
-// 		}
-// 	      else
-// 		{
-// 		  free (word);
-// 		  stream = open_memstream (&word, &size);
-// 		}
-// 	      is_new_line = 0;
-// 	    }
-// 	}
-
-//       else
-// 	{
-// 	  fprintf (stream, "%c", c);
-// 	  fflush (stream);
-// 	}
-//     }
-
-//   token_t token = get_token (word, get_token_type (word), &is_word);
-//   print_token (token);
-//   clean_up_token (&token, &word, &stream, &size);
-
-//   free (word);
-//   fclose (stream);
-
-
-
-
-
-
-
-		// switch (c)
-		// {
-		// 	case '\n':
-		// 	{
-		// 		if (comment_flag)
-		// 		{
-		// 			comment_flag = 0;
-		// 			process_print (word, TOKEN_COMMENT, stream);
-		// 		}
-		// 		else if (pc_set_flag)
-		// 		{
-		// 			switch (pc)
-		// 			{
-		// 				case '<':
-		// 				case '>':
-		// 				case '(':
-		// 				case ')':
-		// 				case '|':
-		// 					break;
-
-		// 				case '&':
-		// 				{
-		// 					if (!and_or_flag)
-		// 					{
-		// 						process_print (word, get_token_type (word), stream);
-		// 					}
-		// 					break;
-		// 				}
-
-		// 				default:
-		// 				{
-		// 					process_print (word, get_token_type (word), stream);
-		// 					break;
-		// 				}
-		// 			}
-		// 		}
-		// 		break;
-		// 	}
-
-		// 	case ' ':
-		// 	{
-		// 		if (comment_flag)
-		// 		{
-		// 			stream_write (stream, c);
-		// 		}
-		// 		else if (pc_set_flag)
-		// 		{
-		// 			switch (pc)
-		// 			{
-		// 				case '<':
-		// 				case '>':
-		// 				case '(':
-		// 				case ')':
-		// 				case '|':
-		// 				case '\n':
-		// 					break;
-
-		// 				case '&':
-		// 				{
-		// 					if (!and_or_flag)
-		// 					{
-		// 						process_print (word, get_token_type (word), stream);
-		// 					}
-		// 					break;
-		// 				}
-
-		// 				default:
-		// 				{
-		// 					process_print (word, get_token_type (word), stream);
-		// 					break;
-		// 				}
-		// 			}
-		// 		}
-		// 		break;
-		// 	}
-
-		// 	case '|':
-		// 	{
-		// 		if (comment_flag)
-		// 		{
-		// 			stream_write (stream, c);
-		// 		}
-		// 		else if (pc_set_flag)
-		// 		{
-		// 			switch (pc)
-		// 			{
-		// 				case '|':
-		// 				{
-		// 					if (and_or_flag)
-		// 					{
-		// 						// report error
-		// 						exit;
-		// 					}
-		// 					else
-		// 					{
-		// 						stream_write (stream, special_char);
-		// 						stream_write (stream, c);
-		// 						process_print (word, get_token_type (word), stream);
-		// 						and_or_flag = 1;
-		// 					}
-		// 					break;
-		// 				}
-
-		// 				case '&':
-		// 				{
-		// 					if (and_or_flag)
-		// 					{
-		// 						stream_write(stream, c);
-		// 						and_or_flag = 0;
-		// 					}
-		// 					else
-		// 					{
-		// 						process_print (word, get_token_type (word), stream);
-		// 						stream_write (stream, c);
-		// 					}
-		// 				}
-
-		// 				case ' ':
-		// 				case '\n':
-		// 				case '<':
-		// 				case '>':
-		// 				case '(':
-		// 				case ')':
-		// 				{
-		// 					stream_write (stream, c);
-		// 					break;
-		// 				}
-
-		// 				default:
-		// 				{
-		// 					process_print (word, get_token_type (word), stream);
-		// 					stream_write (stream, c);
-		// 					break;
-		// 				}
-		// 			}
-		// 		}
-		// 		break;
-		// 	}
-
-		// 	case '&':
-		// 	{
-		// 		if (comment_flag)
-		// 		{
-		// 			stream_write (stream, c);
-		// 		}
-		// 		else if (pc_set_flag)
-		// 		{
-		// 			switch (pc)
-		// 			{
-		// 				case '|':
-		// 				{
-		// 					if (and_or_flag)
-		// 					{
-		// 						stream_write (stream, c);
-		// 						and_or_flag = 0;
-		// 					}
-		// 					else
-		// 					{
-		// 						process_print (word, get_token_type (word), stream);
-		// 						stream_write (stream, c);
-		// 					}
-		// 					break;
-		// 				}
-
-		// 				case '&':
-		// 				{
-		// 					if (and_or_flag)
-		// 					{
-		// 						// report error
-		// 						exit;
-		// 					}
-		// 					else
-		// 					{
-
-		// 						special_flag = 0;
-		// 						and_or_flag = 1;
-		// 					}
-		// 				}
-
-		// 				default:
-		// 				{
-
-		// 				}
-		// 			}
-		// 		}
-		// 		break;
-		// 	}
-
-		// 	case '<':
-		// 	{
-		// 		break;
-		// 	}
-
-		// 	case '>':
-		// 	{
-		// 		break;
-		// 	}
-
-		// 	case '(':
-		// 	{
-		// 		break;
-		// 	}
-
-		// 	case ')':
-		// 	{
-		// 		break;
-		// 	}
-
-		// 	case '#':
-		// 	{
-		// 		comment_flag = 1;
-		// 		break;
-		// 	}
-
-		// 	default:
-		// 	{
-		// 		break;
-		// 	}
-		// }
-
-		// // set pc to be equal to c
-		// pc = c;
-		// // set pc_set_flag to 1
-		// pc_set_flag = 1;
